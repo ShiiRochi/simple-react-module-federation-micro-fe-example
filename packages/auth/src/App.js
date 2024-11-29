bf8218807a05b1d891f4d34d2c80fd2126e7b76c
Denis Bogdanenko <shiirochi@yandex.ru>
2024-11-29 11:30:25 +0300

    add auth subapp

---
+++ packages/auth/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Switch, Route, Router} from "react-router-dom";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "au"
+});
+
+export default function App({history, onSignIn}) {
+    return (
+        <div>
+            <Router history={history}>
+                <StylesProvider generateClassName={generateClassName}>
+                    <Switch>
+                        <Route path="/auth/signin">
+                            <SignIn onSignIn={onSignIn} />
+                        </Route>
+                        <Route path="/auth/signup">
+                            <SignUp onSignIn={onSignIn} />
+                        </Route>
+                    </Switch>
+                </StylesProvider>
+            </Router>
+        </div>
+    );
+}
